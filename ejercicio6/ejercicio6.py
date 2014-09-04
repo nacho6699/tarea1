0dea07215d2ce85f481a53bd6fa8756ea32663ed
nacho <cruzi3836@gmail.com>
2014-09-04 00:56:43 -0400

    ejercicio seis

---
+++ ejercicio6/ejercicio6.py
@@ -0,0 +1,28 @@
+print "intro nro"
+n=int(raw_input())
+i=1
+j=1
+while i<=n:
+    
+	while j<=i:
+		print i,
+		j=j+1
+	
+	print ""
+	print ""
+	if i==n:
+		j=1
+		while j<=i:
+			while i!=0:
+				print i-1,
+				i=i-1
+			j=j+1
+			print ""
+	        print ""
+		break	
+
+	j=1	
+	i=i+1
+
+	
+raw_input()
