cf5fb2126f322db3bdf6d1ff5b088a865628f07b
nacho <cruzi3836@gmail.com>
2014-09-03 21:37:11 -0400

    ejercicio dos modificaddo

--- ejercicio2/ejercicio2.py
+++ ejercicio2/ejercicio2.py
@@ -4,5 +4,11 @@ a=int(raw_input())
 print "inserte rango final"
 b=int(raw_input())
 lista=range(a,b+1)
-print lista[5]
+
+for i in lista:
+	if lista[i]%2==0:
+		lista2[i]=lista[i]
+		
+print lista
+print lista2
 raw_input()
